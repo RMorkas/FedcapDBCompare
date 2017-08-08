1cbbbda18902d8de440d11589238c2d04095a6b9
RMorkas <RMorkas@FedCap.org>
2017-08-08 12:57:42 -0400

    change not null

--- FedcapDBCompare/dbo/Tables/ACC_Project.sql
+++ FedcapDBCompare/dbo/Tables/ACC_Project.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[ACC_Project] (
     [ProjectId]   INT           NOT NULL,
-    [ProjectName] VARCHAR (200) NULL,
+    [ProjectName] VARCHAR (200) NOT NULL,
     CONSTRAINT [PK_ACC_Project] PRIMARY KEY CLUSTERED ([ProjectId] ASC)
 );
 
